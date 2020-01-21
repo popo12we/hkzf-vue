0d0a0b844cb6cfaddcf4bee6f128285c81b659a4
lujunhui <385935631@qq.com>
2020-01-21 15:18:17 +0800

    第一层数据拿到

--- src/pages/Map.vue
+++ src/pages/Map.vue
@@ -4,8 +4,13 @@
 
 <script>
 export default {
-  mounted () {
-    this.initMap()
+  data () {
+    return {
+      // 覆盖物数据
+      overlays: []
+    }
+  },
+  created () {
     this.getHouseData()
   },
   methods: {
@@ -28,52 +33,64 @@ export default {
         cityname
       )
       // 4. 添加文字覆盖物
-      var point = new window.BMap.Point(116.417854, 39.921988)
-      var opts = {
-        // 指定文本标注所在的地理位置
-        position: point,
-        // 设置文本x轴和y轴的偏移量
-        offset: new window.BMap.Size(30, -30)
-      }
-      // 创建文本标注对象
-      var label = new window.BMap.Label(
-        `<div class="bubble">
-            <p class="name">浦东新区</p>
-            <p>388套</p>
+      console.log(this.overlays)
+      this.overlays.forEach(item => {
+        var point = new window.BMap.Point(
+          item.coord.longitude,
+          item.coord.latitude
+        )
+        var opts = {
+          // 指定文本标注所在的地理位置
+          position: point
+          // 设置文本x轴和y轴的偏移量
+          // offset: new window.BMap.Size(30, -30)
+        }
+        // 创建文本标注对象
+        var label = new window.BMap.Label(
+          `<div class="bubble">
+            <p class="name">${item.label}</p>
+            <p>${item.count}套</p>
           </div>`,
-        opts
-      )
-      label.setStyle({
-        width: '70px',
-        height: '70px',
-        lineHeight: '35px',
-        display: 'inline-block',
-        position: 'absolute',
-        borderRadius: '100%',
-        background: 'rgba(12, 181, 106, 0.9)',
-        color: '#fff',
-        border: '2px solid rgba(255, 255, 255, 0.8)',
-        textAlign: 'center',
-        cursor: 'pointer'
+          opts
+        )
+        label.setStyle({
+          width: '50px',
+          height: '50px',
+          lineHeight: '23px',
+          display: 'inline-block',
+          position: 'absolute',
+          borderRadius: '100%',
+          background: 'rgba(12, 181, 106, 0.9)',
+          color: '#fff',
+          border: '2px solid rgba(255, 255, 255, 0.8)',
+          textAlign: 'center',
+          cursor: 'pointer'
+        })
+        // 把文字覆盖物添加到地图上
+        map.addOverlay(label)
       })
-      // 把文字覆盖物添加到地图上
-      map.addOverlay(label)
     },
     // 查询房源数据
     async getHouseData (value) {
-      let res = await this.$axios.get('http://localhost:8080/area/map', {
-        params: {
-          id: value || localStorage.getItem('cityvalue')
+      let { status, body } = await this.$axios.get(
+        'http://localhost:8080/area/map',
+        {
+          params: {
+            id: value || localStorage.getItem('cityvalue')
+          }
         }
-      })
-      console.log(res)
+      )
+      if (status === 200) {
+        this.overlays = body
+        this.initMap()
+      }
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
-  /* 区、镇的覆盖物样式： */
+/* 区、镇的覆盖物样式： */
 
 #l-map {
   height: 100%;
