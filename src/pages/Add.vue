fa51b06c663f5138ea984797d0698cb7d2cc9d22
lujunhui <385935631@qq.com>
2020-01-26 16:25:33 +0800

    样式修改

--- src/pages/Add.vue
+++ src/pages/Add.vue
@@ -10,7 +10,7 @@
         <div class="area-write" @click="goToSearch">
           <i class="cubeic-arrow"></i>
           <div v-if="!communityName">请输入小区名称</div>
-           <div v-if="communityName">{{communityName}}</div>
+          <div v-if="communityName">{{communityName}}</div>
         </div>
       </div>
       <!-- 租金 -->
@@ -29,12 +29,15 @@
           <input v-model="publish.price" type="text" placeholder="请输入建筑面积" />
         </div>
       </div>
-      <!-- 选择面积 -->
+      <!-- 户型 -->
       <div class="choose-measure">
-        请选择
-        <i class="cubeic-arrow"></i>
+        <div class="choose-measure-name">户型</div>
+        <div class="choose-measure-write">
+          <i class="cubeic-arrow"></i>
+          <div>请选择</div>
+        </div>
       </div>
-      <!-- 小区名称 -->
+      <!-- 楼层 -->
       <div class="floor">
         <div class="floor-name">所在楼层</div>
         <div class="floor-write">
@@ -53,14 +56,12 @@
       <!-- 房屋标题 -->
       <div class="house-title">房屋标题</div>
       <div class="house-title-input">
-        <input type="text" placeholder="请输入标题（例如：整租 小区名 2室 5000元）" />
+        <input type="text" v-model="publish.description" placeholder="请输入标题（例如：整租 小区名 2室 5000元）" />
       </div>
       <!-- 图片上传 -->
-      <cube-upload
-  :simultaneous-uploads="3"
-  @file-submitted="fileSubmitted"/>
+      <cube-upload :simultaneous-uploads="3" @file-submitted="fileSubmitted" />
+       <div class="btn" @click="publishData">发布房源</div>
     </div>
-    <button @click="publishData">发布房源</button>
   </div>
 </template>
 
@@ -72,6 +73,34 @@ export default {
   },
   data () {
     return {
+      // 房屋类型
+      roomTypeData: [
+        { label: '一室', value: 'ROOM|d4a692e4-a177-37fd' },
+        { label: '二室', value: 'ROOM|d1a00384-5801-d5cd' },
+        { label: '三室', value: 'ROOM|20903ae0-c7bc-f2e2' },
+        { label: '四室', value: 'ROOM|ce2a5daa-811d-2f49' },
+        { label: '四室+', value: 'ROOM|2731c38c-5b19-ff7f' }
+      ],
+
+      // 朝向：
+      orientedData: [
+        { label: '东', value: 'ORIEN|141b98bf-1ad0-11e3' },
+        { label: '西', value: 'ORIEN|103fb3aa-e8b4-de0e' },
+        { label: '南', value: 'ORIEN|61e99445-e95e-7f37' },
+        { label: '北', value: 'ORIEN|caa6f80b-b764-c2df' },
+        { label: '东南', value: 'ORIEN|dfb1b36b-e0d1-0977' },
+        { label: '东北', value: 'ORIEN|67ac2205-7e0f-c057' },
+        { label: '西南', value: 'ORIEN|2354e89e-3918-9cef' },
+        { label: '西北', value: 'ORIEN|80795f1a-e32f-feb9' }
+      ],
+
+      // 楼层
+      floorData: [
+        { label: '高楼层', value: 'FLOOR|1' },
+        { label: '中楼层', value: 'FLOOR|2' },
+        { label: '低楼层', value: 'FLOOR|3' }
+      ],
+
       communityName: JSON.parse(localStorage.getItem('searchObj')).name || '',
       // 处理图片上传数组
       tempList: [],
@@ -100,7 +129,7 @@ export default {
       const form = new FormData()
       this.tempList.push(e)
       this.tempList.forEach(item => {
-      // 当前接口中要求上传文件的键名为：file
+        // 当前接口中要求上传文件的键名为：file
         form.append('file', item.file)
       })
       const res = await this.$axios.post('/houses/image', form, {
@@ -211,7 +240,22 @@ export default {
     height: 44px;
     line-height: 44px;
     border-bottom: 1px solid #eee;
-    text-align: center;
+    .choose-measure-name {
+      letter-spacing: 2em;
+      width: 50%;
+      float: left;
+    }
+    .choose-measure-write {
+      float: left;
+      width: 50%;
+    }
+    .choose-measure-write {
+      text-align: right;
+      > div,
+      i {
+        float: right;
+      }
+    }
   }
   .floor {
     height: 44px;
@@ -273,5 +317,15 @@ export default {
       color: #ccc;
     }
   }
+  .btn {
+  width: 100%;
+  height: 45px;
+  line-height: 45px;
+  background-color: #21b97a;
+  font-size: 16px;
+  color:#fff;
+  text-align: center;
 }
+}
+
 </style>
